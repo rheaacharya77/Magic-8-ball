9f78d086f2227582a45e1d923dcf8dec7851ad86
Riya Acharya <ria.acharya07@gmail.com>
2021-06-03 15:01:16 +0545

    button functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.lightBlue.shade400,
+        appBar: AppBar(
+          backgroundColor: Colors.grey[850],
+          title: Center(child: Text('Ask Me Anything')),
+        ),
+        body: Ball(),
       ),
-      
     );
   }
 }
 
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+          onPressed: () {
+            setState(
+              () {
+                ballNumber = Random().nextInt(5) + 1;
+                print(ballNumber);
+              },
+            );
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ],
+    );
+  }
+}
